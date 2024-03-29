0251e0b7c8ff51ebcd847141e4a3fe6387a91d44
Henry LETELLIER <henry.letellier@epitech.eu>
2024-03-29 12:24:56 +0100

    [ADD] activated compilation for runner libraries

---
+++ runner_program/libs/curve_fitting/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** point_one_robot_car
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int hello_world(void)
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
