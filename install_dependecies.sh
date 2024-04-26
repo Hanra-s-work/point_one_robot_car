e2ca27d259d44a65090fb211fbfb5b6c2396023b
Victor Yvon <victor.yvon@epitech.eu>
2024-04-26 17:48:08 +0200

    [FEAT]: install script now dl libnop

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -5,3 +5,10 @@ if git submodule update --init --recursive; then
     cmake --build build || exit
   fi
 fi
+prompt=$(sudo -nv 2>&1)
+if [ $? -eq 0 ]; then
+  # exit code of sudo-command is 0
+  sudo apt-get install -y libnop-dev
+else echo $prompt | grep -q '^sudo:'
+  echo "Script must be run with sudo rights"
+fi
