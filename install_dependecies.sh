05b013492f5fac42b8865ecadb28bcd99b417470
Victor Yvon <victor.yvon@epitech.eu>
2024-04-26 17:49:38 +0200

    [FEAT]: install script now dl nlohmann

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -8,7 +8,9 @@ fi
 prompt=$(sudo -nv 2>&1)
 if [ $? -eq 0 ]; then
   # exit code of sudo-command is 0
+  sudo apt-get update
   sudo apt-get install -y libnop-dev
+  sudo apt-get install -y nlohmann-json3-dev
 else echo $prompt | grep -q '^sudo:'
   echo "Script must be run with sudo rights"
 fi
