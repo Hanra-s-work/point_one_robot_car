558ff111fbaf1f017faa4612bc63ae5c95ab8c28
Nizaro <nizarouarti@gmail.com>
2024-04-29 16:40:49 +0200

    update example motor

--- runner_program/pyvesc/examples/motor_example.py
+++ runner_program/pyvesc/examples/motor_example.py
@@ -21,20 +21,30 @@ def run_motor_using_with():
         motor.set_rpm(0)
 
 
+# Nizar todo angle as an input:  relation between full angle 100 and the real angle
+# https://servodatabase.com/servo/traxxas/2056
+# 60 ° range 
+# 30 ° right = 100
+#  0 ° center = 50
+# 30 ° left  = 0 
 # a function to show how to use the class as a static object.
+# angle -30 : 30
+# commande : (angle/30)*50 /100
 def run_motor_as_object():
     motor = VESC(serial_port=serial_port)
     print("Firmware: ", motor.get_firmware_version())
 
     # sweep servo through full range
-    for i in range(100):
+    for i in range(0,100):
+        #if i ==10:
+    
         time.sleep(0.01)
         motor.set_servo(i/100)
 
     # IMPORTANT: YOU MUST STOP THE HEARTBEAT IF IT IS RUNNING BEFORE IT GOES OUT OF SCOPE. Otherwise, it will not
     #            clean-up properly.
-    motor.stop_heartbeat()
-
+        motor.stop_heartbeat()
+    
 
 def time_get_values():
     with VESC(serial_port=serial_port) as motor:
@@ -45,7 +55,7 @@ def time_get_values():
 
 
 if __name__ == '__main__':
-    run_motor_using_with()
+    #run_motor_using_with()
     run_motor_as_object()
-    time_get_values()
+    #time_get_values()
 
